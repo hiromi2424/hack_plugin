2be70853fcd6def844c7568fc323cee89f423cab
hiromi2424 <tanaka2424@gmail.com>
2010-05-28 20:48:45 +0900

    Initial commit.

---
+++ plugins/hack/controllers/components/alias.php
@@ -0,0 +1,54 @@
+<?php
+class AliasComponent{
+	function initialize(&$controller, $settings = array()){
+		if(!is_array($settings) || empty($settings)){
+			return;
+		}
+		
+		$Component =& $controller->Component;
+		$loaded =& $Component->_loaded;
+		
+		foreach($settings as $from => $to){
+			$from = Inflector::camelize($from);
+			$to   = Inflector::camelize($to);
+			
+			list($plugin, $name) = pluginSplit($to);
+			$class = $name . 'Component';
+			
+			if(!class_exists($class)){
+				if(!App::import('Component', $to)){
+					continue;
+				}
+			}
+			$object = null;
+			if(is_subclass_of($class, 'SessionComponent')){
+				$object = new $class($controller->base);
+			}else{
+				$object = new $class;
+			}
+			// component needs to have "enabled" parameter
+			$object->enabled = isset($loaded[$from]) ? $loaded[$from]->enabled : true;
+			
+			if(method_exists($object, 'initialize')){
+				//See current method timing to prevent the component from being initialized twice.
+				$order = array_flip(array_keys($loaded));
+				$will_be_initialized =
+					isset($order[$from]) &&
+					($order['Alias'] < $order[$from])
+				;
+				
+				if(!$will_be_initialized){
+					// Get original settings
+					$options = array();
+					if(isset($Component->__settings[$from])){
+						$options = $Component->__settings[$from];
+					}
+					$object->initialize($controller, $options);
+				}
+			}
+			
+			// must not do =&
+			$loaded[$from] = $object;
+		}
+	}
+}
---
+++ plugins/hack/views/helpers/alias.php
@@ -0,0 +1,43 @@
+<?php
+class AliasHelper extends AppHelper{
+	var $unset = true;
+	var $settings = array();
+	var $view;
+	
+	function beforeRender(){
+		if(is_object($this->view)){
+			foreach($this->settings as $from => $to){
+				$this->_replace($from, $to);
+			}
+		}
+		parent::beforeRender();
+	}
+	
+	function __construct($settings = array()){
+		$this->view =& ClassRegistry::getObject('view');
+		$this->settings = array_merge($this->settings, $settings);
+	}
+	
+	function _replace($from, $to){
+		$unset = $this->unset;
+		if(is_array($to)){
+			extract($to);
+		}
+		
+		$from = Inflector::variable($from);
+		$to   = Inflector::variable($to);
+		
+		$this->view->_loadHelpers($this->view->_loaded, array($to));
+		
+		list($plugin, $to) = pluginSplit($to);
+		
+		if(isset($this->view->loaded[$from]) && isset($this->view->loaded[$to])){
+			$this->view->loaded[$from] = $this->view->loaded[$to];
+			if($unset){
+				unset($this->view->loaded[$to]);
+			}
+			return true;
+		}
+		return false;
+	}
+}
