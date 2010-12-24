bdac5e7f9bd89944defce269ae83385614d67ff8
hiromi2424 <tanaka2424@gmail.com>
2010-12-24 14:41:13 +0900

    now supports loading components on the fly, specify settings within alias setting.

--- controllers/components/alias.php
+++ controllers/components/alias.php
@@ -1,61 +1,104 @@
 <?php
 class AliasComponent{
-	function initialize(&$controller, $settings = array()){
+	var $Controller;
+	var $__primary;
+
+	function initialize($controller, $settings = array()){
+		$this->Controller = $controller;
+
 		if (!is_array($settings) || empty($settings)) {
 			return;
 		}
 
-		$Component =& $controller->Component;
+		$this->__primary = true;
+		$this->loadComponents($settings, false);
+		$this->__primary = false;
+	}
+
+	function _loadComponent($from, $to, $settings, $startup = true) {
+		$Component =& $this->Controller->Component;
 		$loaded =& $Component->_loaded;
 
-		foreach ($settings as $from => $to) {
-			$from = Inflector::camelize($from);
-			$to   = Inflector::camelize($to);
+		$from = Inflector::camelize($from);
+		$to   = Inflector::camelize($to);
 
-			list($plugin, $name) = pluginSplit($to);
-			$class = $name . 'Component';
+		list($plugin, $name) = pluginSplit($to);
+		$class = $name . 'Component';
 
-			if (!class_exists($class)) {
-				if (!App::import('Component', $to)) {
-					continue;
-				}
-			}
-
-			if (is_subclass_of($class, 'SessionComponent')) {
-				$object = new $class($controller->base);
-			} else {
-				$object = new $class;
-			}
-			// component needs to have "enabled" parameter
-			$object->enabled = isset($loaded[$from]) ? $loaded[$from]->enabled : true;
-			if (!isset($object->components)) {
-				$object->components = array();
-			}
-			$Component->_loadComponents($object, $name);
-
-			if (method_exists($object, 'initialize')) {
-				//See current method timing to prevent the component from being initialized twice.
-				$order = array_flip(array_keys($loaded));
-				$will_be_initialized =
-					isset($order[$from]) &&
-					($order['Alias'] < $order[$from])
-				;
-
-				if (!$will_be_initialized) {
-					// Get original settings
-					$options = array();
-					if (isset($Component->__settings[$from])) {
-						$options = $Component->__settings[$from];
-					}
-					$object->initialize($controller, $options);
-				}
-			}
-
-			// must not do =&
-			$loaded[$from] = $object;
-			if (!isset($controller->$from)) {
-				$controller->$from =& $loaded[$from];
+		if (!class_exists($class)) {
+			if (!App::import('Component', $to)) {
+				return false;
 			}
 		}
+
+		if ($class === 'SessionComponent' || is_subclass_of($class, 'SessionComponent')) {
+			$object = new $class($this->Controller->base);
+		} else {
+			$object = new $class;
+		}
+		// component needs to have "enabled" parameter
+		$object->enabled = isset($loaded[$from]) ? $loaded[$from]->enabled : true;
+		if (!isset($object->components)) {
+			$object->components = array();
+		}
+		$Component->_loadComponents($object, $name);
+
+		if (method_exists($object, 'initialize')) {
+			//See current method timing to prevent the component from being initialized twice.
+			$order = array_flip(array_keys($loaded));
+			$will_be_initialized =
+				isset($order[$from]) &&
+				($order['Alias'] < $order[$from])
+			;
+
+			if ($startup || !$will_be_initialized) {
+				// Get original settings
+				if (isset($Component->__settings[$from])) {
+					$settings = Set::merge($Component->__settings[$from], $settings);
+				}
+				$Component->__settings[$from] = $settings;
+				$object->initialize($this->Controller, $settings);
+			}
+		}
+
+		if (!$this->__primary && $startup && method_exists($object, 'startup')) {
+			$object->startup($this->Controller);
+		}
+
+		// must not do =&
+		$loaded[$from] = $object;
+
+		if ($this->__primary && !isset($this->Controller->$from) && !in_array($from, $Component->_primary)) {
+			$Component->_primary[] = $from;
+		}
+
+		if (!isset($this->Controller->$from) || get_class($this->Controller->$from) !== $class) {
+			$this->Controller->$from = $loaded[$from];
+		}
+		return true;
+	}
+
+	function loadComponent($component, $settings = array(), $startup = true) {
+		return $this->_loadComponent($component, $component, $settings, $startup);
+	}
+
+	function loadComponents($components, $startupDefault = true) {
+		foreach ($components as $from => $alias) {
+			list($to, $settings) = each(Set::normalize($alias));
+			if (empty($settings)) {
+				$settings = array();
+			}
+
+			$startup = $startupDefault;
+			if (!$this->__primary && isset($settings['startup'])) {
+				$startup = $settings['startup'];
+				unset($settings['startup']);
+			}
+			$this->_loadComponent($from, $to, $settings, $startup);
+		}
+	}
+
+	function loadAlias($from, $to, $settings = array(), $startup = true) {
+		return $this->_loadComponent($from, $to, $settings, $startup);
 	}
 }
--- tests/cases/controllers/components/alias.test.php
+++ tests/cases/controllers/components/alias.test.php
@@ -5,10 +5,10 @@ App::import('Controller', array('Component', 'Controller'), false);
 class AliasComponentTestController extends Controller {
 	var $name = 'AliasComponentTest';
 	var $components = array(
-		'Cookie',
 		'HackPlugin.Alias' => array(
 			'Cookie' => 'ExtendedCookie',
 		),
+		'Cookie',
 	);
 	var $uses = null;
 }
@@ -18,6 +18,22 @@ class ExtendedCookieComponent extends CookieComponent {
 	
 }
 
+class HackAliasTestComponent extends Object {
+	var $hoge;
+	var $piyo;
+	var $fuga;
+	function initialize($controller, $settings = array()) {
+		$this->_set($settings);
+	}
+}
+
+class HackAliasTestOverrideComponent extends HackAliasTestComponent {
+	function startup() {
+		$this->hoge = 3;
+	}
+}
+
+
 class AliasTestCase extends CakeTestCase {
 	var $Controller = null;
 
@@ -25,20 +41,24 @@ class AliasTestCase extends CakeTestCase {
 		$this->_reset();
 	}
 
-	function _reset($components = null, $unsets = null) {
+	function _reset($sets = null, $unsets = null) {
 		unset($this->Controller);
 		$this->Controller = new AliasComponentTestController();
 
-		if ($components !== null) {
-			$this->Controller->components = Set::merge($this->Controller->components, $components);
-		}
 		if ($unsets !== null) {
-			$components = Set::normalize($this->Controller->components);
-			$this->Controller->components = Set::diff($components, array_flip((array)$unsets));
+			if ($unsets === true) {
+				$this->Controller->components = array();
+			} else {
+				$components = Set::normalize($this->Controller->components);
+				$this->Controller->components = Set::diff($components, array_flip((array)$unsets));
+			}
 		}
-
+		if ($sets !== null) {
+			$this->Controller->components = Set::merge($this->Controller->components, $sets);
+		}
 		$this->Controller->constructClasses();
-		$this->Controller->startupProcess();
+		$this->Controller->Component->initialize($this->Controller);
+		$this->Controller->beforeFilter();
 	}
 
 	function endTest() {
@@ -47,10 +67,83 @@ class AliasTestCase extends CakeTestCase {
 
 	function testOverride() {
 		$this->assertEqual(get_class($this->Controller->Cookie), 'ExtendedCookieComponent');
+
+		$this->_reset(array(
+			'HackAliasTest' => array(
+				'piyo' => 1,
+			),
+			'HackPlugin.Alias' => array(
+				'HackAliasTest' => array(
+					'HackAliasTestOverride' => array(
+						'fuga' => 2,
+					),
+				),
+			),
+		), true);
+		$this->assertEqual(get_class($this->Controller->HackAliasTest), 'HackAliasTestOverrideComponent');
+		$this->assertEqual($this->Controller->HackAliasTest->piyo, 1);
+		$this->assertEqual($this->Controller->HackAliasTest->fuga, 2);
+		$this->assertNull($this->Controller->HackAliasTest->hoge);
+
+		$this->Controller->Component->triggerCallback('startup', $this->Controller);
+		$this->assertEqual($this->Controller->HackAliasTest->hoge, 3);
 	}
 
 	function testAppend() {
 		$this->_reset(null, 'Cookie');
 		$this->assertEqual(get_class($this->Controller->Cookie), 'ExtendedCookieComponent');
+
+		$this->_reset(array(
+			'HackPlugin.Alias' => array(
+				'HackAliasTest' => array(
+					'HackAliasTestOverride' => array(
+						'fuga' => 2,
+					),
+				),
+			),
+		), true);
+		$this->assertEqual(get_class($this->Controller->HackAliasTest), 'HackAliasTestOverrideComponent');
+		$this->assertEqual($this->Controller->HackAliasTest->fuga, 2);
+		$this->assertNull($this->Controller->HackAliasTest->hoge);
+
+		$this->Controller->Component->triggerCallback('startup', $this->Controller);
+		$this->assertEqual($this->Controller->HackAliasTest->hoge, 3);
+	}
+
+	function testLoadMethods() {
+		$this->_reset('HackPlugin.Alias', true);
+		$this->assertTrue($this->Controller->Alias->loadComponent('HackAliasTest', array('piyo' => 1)));
+		$this->assertEqual(get_class($this->Controller->HackAliasTest), 'HackAliasTestComponent');
+		$this->assertEqual($this->Controller->HackAliasTest->piyo, 1);
+
+		$this->assertTrue($this->Controller->Alias->loadAlias('HackAliasTest', 'HackAliasTestOverride', array('fuga' => 2)));
+		$this->assertEqual(get_class($this->Controller->HackAliasTest), 'HackAliasTestOverrideComponent');
+		$this->assertEqual($this->Controller->HackAliasTest->piyo, 1);
+		$this->assertEqual($this->Controller->HackAliasTest->fuga, 2);
+		$this->assertEqual($this->Controller->HackAliasTest->hoge, 3);
+		$this->assertFalse($this->Controller->Alias->loadComponent('NotDefined'));
+
+		$this->_reset('HackPlugin.Alias', true);
+		$this->Controller->Alias->loadComponents(array(
+			'HackAliasTest' => array(
+				'HackAliasTestOverride' => array(
+					'fuga' => 2,
+					'startup' => false,
+				),
+			),
+			'Cookie' => 'ExtendedCookie',
+		));
+		$this->assertEqual(get_class($this->Controller->HackAliasTest), 'HackAliasTestOverrideComponent');
+		$this->assertEqual($this->Controller->HackAliasTest->fuga, 2);
+		$this->assertNull($this->Controller->HackAliasTest->hoge);
+		$this->assertEqual(get_class($this->Controller->Cookie), 'ExtendedCookieComponent');
+	}
+
+	function testSessionComponent() {
+		$this->_reset('HackPlugin.Alias', true);
+		$this->Controller->base = '/base';
+		$this->assertTrue($this->Controller->Alias->loadComponent('Session'));
+		$this->assertEqual(get_class($this->Controller->Session), 'SessionComponent');
+		$this->assertEqual($this->Controller->Session->path, '/base');
 	}
 }
