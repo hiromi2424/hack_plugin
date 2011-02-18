f3848c16c5fe1ca36f470b03c16f3b284e2ca707
hiromi2424 <tanaka2424@gmail.com>
2011-02-18 16:33:33 +0900

    Alias Helper now can be used correctly

--- tests/cases/views/helpers/alias.test.php
+++ tests/cases/views/helpers/alias.test.php
@@ -1,13 +1,64 @@
 <?php
 
-App::import('Helper', 'Hack.Alias');
+App::import('Helper', 'Html');
+
+class AliasTestHtmlHelper extends HtmlHelper {
+}
 
 class AliasHelperTestCase extends CakeTestCase {
+
 	function startTest() {
-		$this->Helper = new AliasHelper;
+		$this->View = new View(new Controller);
 	}
 
 	function endTest() {
-		unset($this->Helper);
+		unset($this->View);
+		ClassRegistry::flush();
 	}
+
+	function _reset($settings = array()) {
+		$this->endTest();
+		$this->startTest();
+		$this->View->helpers = array('Hack.Alias' => $settings);
+		$out = $this->_render();
+	}
+
+	function _render() {
+		$this->View->_render(LIBS . 'view' . DS . 'pages' . DS . 'home.ctp', array());
+	}
+
+	function testReplace() {
+
+		$this->_reset(array(
+			'Html' => 'AliasTestHtml',
+		));
+
+		$this->assertTrue(isset($this->View->Html));
+		$this->assertFalse(isset($this->View->html));
+		$this->assertFalse(isset($this->View->AliasTestHtml));
+		$this->assertFalse(isset($this->View->loaded['AliasTestHtml']));
+		$this->assertFalse(isset($this->View->loaded['aliasTestHtml']));
+		$this->assertIsA($this->View->Html, 'AliasTestHtmlHelper');
+		$this->assertIsA($this->View->loaded['html'], 'AliasTestHtmlHelper');
+
+		$this->_reset(array(
+			'Html' => 'aliasTestHtml',
+		));
+
+		$this->assertTrue(isset($this->View->Html));
+		$this->assertFalse(isset($this->View->html));
+		$this->assertFalse(isset($this->View->AliasTestHtml));
+		$this->assertFalse(isset($this->View->aliasTestHtml));
+
+		$this->_reset(array(
+			'html' => 'aliasTestHtml',
+		));
+
+		$this->assertTrue(isset($this->View->Html));
+		$this->assertFalse(isset($this->View->html));
+		$this->assertFalse(isset($this->View->AliasTestHtml));
+		$this->assertFalse(isset($this->View->aliasTestHtml));
+
+	}
+
 }
--- views/helpers/alias.php
+++ views/helpers/alias.php
@@ -3,41 +3,41 @@ class AliasHelper extends AppHelper {
 	var $unset = true;
 	var $settings = array();
 	var $view;
-	
+
 	function beforeRender(){
-		if(is_object($this->view)){
-			foreach($this->settings as $from => $to){
-				$this->_replace($from, $to);
+		if (is_object($this->view)) {
+			foreach ($this->settings as $from => $alias) {
+				$this->_replace($from, $alias);
 			}
 		}
 		parent::beforeRender();
 	}
-	
+
 	function __construct($settings = array()){
 		$this->view =& ClassRegistry::getObject('view');
 		$this->settings = array_merge($this->settings, $settings);
 	}
-	
-	function _replace($from, $to){
-		$unset = $this->unset;
-		if(is_array($to)){
-			extract($to);
-		}
-		
-		$from = Inflector::variable($from);
-		$to   = Inflector::variable($to);
-		
-		$this->view->_loadHelpers($this->view->loaded, array($to));
-		
+
+	function _replace($from, $alias){
+
+		list($to, $settings) = each(Set::normalize((array)$alias));
+
+		$loadedHelpers = array();
+		$this->view->_loadHelpers($loadedHelpers, array($to => $settings));
+
 		list($plugin, $to) = pluginSplit($to);
-		
-		if(isset($this->view->loaded[$from]) && isset($this->view->loaded[$to])){
-			$this->view->loaded[$from] = $this->view->loaded[$to];
-			if($unset){
-				unset($this->view->loaded[$to]);
-			}
-			return true;
+		if (!isset($loadedHelpers[$to])) {
+			return false;
 		}
-		return false;
+		$loaded =& $loadedHelpers[$to];
+
+		list($plugin, $from) = pluginSplit($from);
+		$key = Inflector::variable($from);
+		$from = Inflector::classify($from);
+
+		$this->view->loaded[$key] = $loaded;
+		$this->view->$from =& $loaded;
+
+		return true;
 	}
 }
